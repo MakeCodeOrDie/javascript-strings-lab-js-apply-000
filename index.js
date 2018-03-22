090434120f75652e450e35320c0692256ea00368
Jason Pollack <jasonpollack87@gmail.com>
2018-03-22 23:29:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello, everybody!";
-
+var greet= "Hello";
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = greeting + specialGuest + "!";
+var greetSpecialGuest = greet + specialGuest + "!";
 
 var topic = "Let's talk about space";
 
